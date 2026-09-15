a8a867015e2e89ca73d02df9fa6493de91baf16d
Rachel Liang <rltimes10@gmail.com>
2026-09-15 06:19:32 -0400

    Added review reporting feature

--- movies/views.py
+++ movies/views.py
@@ -52,6 +52,14 @@ def edit_review(request, id, review_id):
         return redirect('movies.show', id=id)
 
 @login_required
+def report_review(request, id, review_id):
+    if request.method == 'POST' :
+        review = get_object_or_404(Review, id = review_id)
+        review.delete()
+    return redirect('movies.show', id = id)
+
+
+@login_required
 def delete_review(request, id, review_id):
     review = get_object_or_404(Review, id=review_id, user=request.user)
     review.delete()
